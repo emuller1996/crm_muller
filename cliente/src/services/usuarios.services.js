99bebb61e01a24bf2f06f5ebba57cace39f1df2a
emuller1996 <stefano.muller@hotmail.com>
2026-03-06 22:21:47 -0500

    feat: se agrega el use de usuario funcionalidades de  roles

--- cliente/src/services/usuarios.services.js
+++ cliente/src/services/usuarios.services.js
@@ -10,6 +10,22 @@ export const getAllUsuariosService = (token, signal) => {
   return axios.get('/usuarios', { headers: { 'access-token': token }, signal })
 }
 
+export const patchUpdateUsuariosService = (token, data,id) => {
+  return axios.patch(`/usuarios/${id}`, data, { headers: { 'access-token': token } })
+}
+
 export const patchChangePasswordUsuariosService = (token, data,id) => {
   return axios.patch(`/usuarios/${id}/change_password`, data, { headers: { 'access-token': token } })
+}
+
+export const postCreateRoleService = (token, data) => {
+  return axios.post('/roles', data, { headers: { 'access-token': token } })
+}
+
+export const getAllRoleService = (token, signal) => {
+  return axios.get('/roles', { headers: { 'access-token': token }, signal })
+}
+
+export const putChangeRoleService = (token, data,id) => {
+  return axios.put(`/roles/${id}`, data, { headers: { 'access-token': token } })
 }
