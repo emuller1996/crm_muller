3f2008b20a601974c39cf2ca813f45a64f3ed0b6
emuller1996 <stefano.muller@hotmail.com>
2026-03-26 23:33:25 -0500

    feat(Factuas) : add sea agrega logica de roles en facturas

--- servidor/src/modules/facturas/facturas.routes.js
+++ servidor/src/modules/facturas/facturas.routes.js
@@ -1,19 +1,20 @@
 import { Router } from "express";
 import * as controller from "./facturas.controller.js";
 import { validateTokenMid } from "../../utils/authjws.js";
+import { checkPermission } from "../../middleware/acl.middleware.js";
 
 const router = Router();
 
-router.get("/", controller.getAll);
-router.get("/:id", controller.getById);
-router.get("/per_day/:date", controller.getPerDay);
+router.get("/", checkPermission("facturas.read"),controller.getAll);
+router.get("/:id", checkPermission("facturas.read"),controller.getById);
+router.get("/per_day/:date",checkPermission("facturas.read"), controller.getPerDay);
 
-router.post("/", validateTokenMid, controller.create);
-router.put("/:id", validateTokenMid, controller.update);
+router.post("/", checkPermission("facturas.create"), controller.create);
+router.put("/:id", checkPermission("facturas.update"), controller.update);
 
-router.patch("/:id/anular", validateTokenMid, controller.anularFactura);
+router.patch("/:id/anular", checkPermission("facturas.update"), controller.anularFactura);
 
-router.post("/:id/pagos", controller.createPago);
-router.get("/:id/pagos", controller.getPagos);
+router.post("/:id/pagos",checkPermission("facturas.create"), controller.createPago);
+router.get("/:id/pagos",checkPermission("facturas.read"), controller.getPagos);
 
 export default router;
--- servidor/src/modules/facturas_compra/facturas_compra.routes.js
+++ servidor/src/modules/facturas_compra/facturas_compra.routes.js
@@ -1,19 +1,20 @@
 import { Router } from "express";
 import * as controller from "./facturas_compra.controller.js";
 import { validateTokenMid } from "../../utils/authjws.js";
+import { checkPermission } from "../../middleware/acl.middleware.js";
 
 const router = Router();
 
-router.get("/", controller.getAll);
-router.get("/:id", controller.getById);
-router.get("/per_day/:date", controller.getPerDay);
+router.get("/",checkPermission("facturas_compra.read"), controller.getAll);
+router.get("/:id",checkPermission("facturas_compra.read"), controller.getById);
+router.get("/per_day/:date",checkPermission("facturas_compra.read"), controller.getPerDay);
 
-router.post("/", validateTokenMid, controller.create);
-router.put("/:id", validateTokenMid, controller.update);
+router.post("/", checkPermission("facturas_compra.create"), controller.create);
+router.put("/:id", checkPermission("facturas_compra.update"), controller.update);
 
-router.patch("/:id/anular", validateTokenMid, controller.anularFactura);
+router.patch("/:id/anular", checkPermission("facturas_compra.update"),controller.anularFactura);
 
-router.post("/:id/pagos", controller.createPago);
-router.get("/:id/pagos", controller.getPagos);
+router.post("/:id/pagos",checkPermission("facturas_compra.create"), controller.createPago);
+router.get("/:id/pagos", checkPermission("facturas_compra.read"),controller.getPagos);
 
 export default router;
