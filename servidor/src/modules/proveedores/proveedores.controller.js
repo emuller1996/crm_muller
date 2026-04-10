918cacb157711417ba4dd8975b34bc772b5ed377
emuller1996 <stefano.muller@hotmail.com>
2026-04-09 23:00:10 -0500

    feat(Proveedores): se agrega filtrado por empresa id

--- servidor/src/modules/proveedores/proveedores.controller.js
+++ servidor/src/modules/proveedores/proveedores.controller.js
@@ -3,7 +3,7 @@ import { jwtDecode } from "jwt-decode";
 
 export const getAll = async (req, res) => {
   try {
-    const proveedores = await service.getAll();
+    const proveedores = await service.getAll(req.empresaId);
     return res.status(200).json(proveedores);
   } catch (error) {
     return res.status(500).json({ message: error.message });
@@ -12,7 +12,7 @@ export const getAll = async (req, res) => {
 
 export const pagination = async (req, res) => {
   try {
-    const result = await service.pagination(req.query);
+    const result = await service.pagination({ ...req.query, empresa_id: req.empresaId });
     return res.status(200).json(result);
   } catch (error) {
     return res.status(500).json({ message: error.message });
@@ -32,6 +32,7 @@ export const create = async (req, res) => {
   try {
     const data = req.body;
     data.user_create_id = jwtDecode(req.headers["access-token"])?._id;
+    data.empresa_id = req.empresaId;
     data.createdTime = Date.now();
 
     const proveedor = await service.create(data);
