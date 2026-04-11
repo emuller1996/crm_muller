4d7d799fdba3bccef1002af7d3363f077782b238
emuller1996 <stefano.muller@hotmail.com>
2026-04-10 23:39:26 -0500

    feat(Permisos) : add permisos a cliente y roles

--- servidor/src/modules/clientes/clientes.routes.js
+++ servidor/src/modules/clientes/clientes.routes.js
@@ -1,23 +1,24 @@
 import { Router } from "express";
 import * as controller from "./clientes.controllers.js";
 import { validateTokenMid } from "../../utils/authjws.js";
+import { checkPermission } from "../../middleware/acl.middleware.js";
 
 const router = Router();
 
-router.get("/", validateTokenMid, controller.getAll);
+router.get("/", validateTokenMid,checkPermission("clientes.read"), controller.getAll);
 
-router.get("/pagination", controller.pagination);
+router.get("/pagination",checkPermission("clientes.read"), controller.pagination);
 
-router.get("/:id", controller.getById);
+router.get("/:id",checkPermission("clientes.read"), controller.getById);
 
-router.post("/", controller.create);
+router.post("/",checkPermission("clientes.create"), controller.create);
 
-router.put("/:id", controller.update);
+router.put("/:id", checkPermission("clientes.update"), controller.update);
 
-router.post("/:id/comments", controller.createComment);
+router.post("/:id/comments",  checkPermission("clientes.create"), controller.createComment);
 
-router.get("/:id/comments", controller.getComments);
+router.get("/:id/comments",checkPermission("clientes.read"), controller.getComments);
 
-router.post("/import-excel", controller.importExcel);
+router.post("/import-excel",checkPermission("clientes.create"), controller.importExcel);
 
 export default router;
--- servidor/src/modules/roles/roles.routes.js
+++ servidor/src/modules/roles/roles.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as controller from "./roles.controllers.js";
+import { checkPermission } from "../../middleware/acl.middleware.js";
 
 const router = Router();
 
-router.get("/", controller.getAll);
-router.post("/", controller.create);
-router.put("/:id", controller.update);
+router.get("/", checkPermission("usuarios.read"),controller.getAll);
+router.post("/",checkPermission("usuarios.create"), controller.create);
+router.put("/:id",checkPermission("usuarios.update"), controller.update);
 
 export default router;
