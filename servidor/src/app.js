3fd7403712117e9825848bc289e31246f1bc5cc2
emuller1996 <stefano.muller@hotmail.com>
2026-05-21 10:34:05 -0500

    feat : add new URL front in cors config

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -15,7 +15,8 @@ const server = express();
 const corsOptions = {
     origin:[
         'https://crm.esmuller.cloud',
-        'https://crm-muller-admin.vercel.app'
+        'https://crm-muller-admin.vercel.app',
+        'https://crm-muller.vercel.app'
     ],
     credentials: true,
     optionsSuccessStatus: 200
@@ -25,9 +26,12 @@ server.use(cors(corsOptions));
 
 // Middleware para asegurar CORS incluso en errores
 server.use((err, req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://crm.esmuller.cloud');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next(err);
+    if (err.name === 'CORSError') {
+        console.error('CORS Error:', err.message);
+        res.status(403).json({ error: 'CORS policy violation' });
+    } else {
+        next(err);
+    }
 });
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
